2682e836679e9c96292f4e9b3c9737c10c9e9bc8
0zy0 <1589219471@qq.com>
2026-03-20 14:43:43 +0800

    分类管理模块代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -137,7 +137,7 @@ public class EmployeeController {
      */
     @PutMapping
     @ApiOperation("编辑员工信息")
-    public Result update(@RequestBody EmployeeDTO  employeeDTO){
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
         log.info("编辑员工信息");
         employeeService.update(employeeDTO);
         return Result.success();
