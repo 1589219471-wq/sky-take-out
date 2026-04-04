2535650326d7d6f88b899fa7764b323d38445950
0zy0 <1589219471@qq.com>
2026-04-05 00:48:05 +0800

    用户数据统计功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -4,6 +4,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -39,5 +40,20 @@ public class ReportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate end){
         log.info("营业额统计：{}，{}", begin, end);
         return Result.success(reportService.getTurnOverStatistics(begin, end));
+    }
+
+    /**
+     * 用户统计
+     * @param begin 开始日期
+     * @param end 结束日期
+     * @return 用户统计数据
+     */
+    @GetMapping("/userStatistics")
+    @ApiOperation("用户统计")
+    public Result<UserReportVO> userStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
+        log.info("用户数据统计：{},{}", begin, end);
+        return Result.success(reportService.getUserStatistics(begin, end));
     }
 }
